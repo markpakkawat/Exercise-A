957c0dae41ececfcdc6edceb5b52f31f9bf2ad2d
markpakkawat <98144346+markpakkawat@users.noreply.github.com>
2022-05-02 17:01:53 +0700

    A02 Simple Node Module

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
-var rect = {
-    perimeter: (x, y) => (2*(x+y)),
-    area: (x, y) => (x*y)
-};
+var rect = require("./rectangle");
 
 function solveRect(l,b) {
-    console.log("Solving for rectangle with 1 = "+1+" and b = "+b);
+    console.log("Solving for rectangle with l = "+l+" and b = "+b);
 
     if(l <= 0 || b <= 0){
-        console.log("Rectangle dimensions should be greater then zero: 1 = "+1+" , and b = "+b);
+        console.log("Rectangle dimensions should be greater then zero: l = "+l+" , and b = "+b);
     }
     else {
         console.log("The area of the rectangle is "+rect.area(l,b));
